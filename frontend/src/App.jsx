02d19cc6754fefd6f9de3765943b41479a6ba9e7
privateluvaccess-png <privateluvaccess@gmail.com>
2026-08-01 10:19:24 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-// trigger redeploy
-
 import React, { useEffect, useState, useRef } from 'react';
 import { LanguageProvider, useLanguage } from './i18n/LanguageContext';
 import Feed from './components/Feed';
